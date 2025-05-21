99b7a642f764bfc1cdcba3eebde2f517b1ae9341
fogelmemory123 <fogelmemory123@gmail.com>
2025-05-21 18:32:40 +0300

    Update config.py

--- m-system/backEnd/config.py
+++ m-system/backEnd/config.py
@@ -1,7 +1,7 @@
 import os
 from datetime import timedelta
 class Config:
-    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI = "postgresql://username:password@host:port/database_name"
+    SQLALCHEMY_DATABASE_URI = "postgresql://username:password@host:port/database_name"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', '')
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=90)
